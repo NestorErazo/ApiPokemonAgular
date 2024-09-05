89be5ba245f6d2669c814a66d8667b42e209978e
NestorErazo <njerazo@gmail.com>
2024-09-05 17:07:08 -0500

    Add files via upload

--- src/app/componentes/lista/lista.component.ts
+++ src/app/componentes/lista/lista.component.ts
@@ -1,51 +1,48 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PokeapiService } from '../../servicios/pokeapi.service';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
-
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [HttpClientModule,MatCardModule, MatButtonModule],
-  providers: [PokeapiService],
+  imports: [HttpClientModule, MatCardModule, MatButtonModule],
+  providers:[PokeapiService],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss'
 })
-export class ListaComponent implements OnInit {
-  listapokemones: any;
-  pokemonesCompleto: any[] = []
-  constructor(private pokeapi:PokeapiService){}
+export class ListaComponent implements OnInit{
+  listaPokemones: any;
+  pokemonesCompleto : any []=[]
+  constructor(private pokeApi: PokeapiService) { }
   
   ngOnInit(): void {
-    this.pokeapi.obtenerListadoPokemones().subscribe({
-        next: (data: any)=>{
-          this.listapokemones = data;
-          this.listapokemones.results.forEach( (element: any) => {
-          this.pokeapi.obtenerListadoPokemones(element.url).subscribe({
-            next: (data: any) => {
+    this.pokeApi.obtenerListadoPokemones().subscribe({
+      next: (data: any) => {
+
+        this.listaPokemones = data;
+        this.listaPokemones.results.forEach((element:any) => {
+          this.pokeApi.obtenerUnPokemon(element.url).subscribe({
+            next: (data: any) =>{
               this.pokemonesCompleto.push(data)
-            console.log(this.pokemonesCompleto);  
-          },
-        })
+            },
+          })
         });
-      console.log(this.listapokemones);
-      console.log(this.pokemonesCompleto)
-       },
-         error: (error: any) => {console.log(error)}
-        })
-      }
-      
-
-nextPage(nextUrl: string): void { }
-
-playSound(soundSource: string){
-  const audio = new Audio();
-  audio.src = soundSource;
-  audio.load();
-  audio.play();
-    
-    }
+        console.log(this.listaPokemones)
+        console.log(this.pokemonesCompleto)
+      },
+      error:(err:any) => { console.log(err)}
+   })
   }
   
+  nextPage(nextUrl: string): void {}
+
+playSound(soundSource: string){
+const audio = new Audio();
+audio.src = soundSource;
+audio.load();
+audio.play();
+  }
+  
+}
