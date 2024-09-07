8574f0e83ba047d57bc56e81eef235377cd4a7c4
njerazo <njerazo@gmail.com>
2024-09-06 23:14:29 -0500

    first commit3

--- app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListaComponent } from './componentes/lista/lista.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: ListaComponent
-    }
-];
